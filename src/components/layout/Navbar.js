5d6cf24deb9c0c8717459d304eaf0a70cf22e5d1
Felix Bengtsson <felix.bengtsson@me.com>
2019-10-05 19:50:48 +0200

    built lyrics app

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-5">
+    
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div style={{ margin: '0 auto' }}>
+      Loading...
+    </div>
+  )
+}
